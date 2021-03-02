733bb13c082f6d4dae1de126c1f7fa3f35e1e915
Atilin <71184528+Atilin@users.noreply.github.com>
2021-03-02 18:58:50 +0300

    Update Source.cpp

--- 2021.02.24-Homework-2/2021.02.24-Homework-2/Source.cpp
+++ 2021.02.24-Homework-2/2021.02.24-Homework-2/Source.cpp
@@ -2,78 +2,89 @@
 
 using namespace std;
 
-//int resolveOpNumber(char op);
-template<class T1, class T2> //подбирает тип переменной под передаваемое значение
-T1 sum(T1 a, T2 b)
-{
-	return a + b;
-}
-
-template<class T1, class T2>
-T1 diff(T1 a, T2 b)
-{
-	return a - b;
-}
-
-template<class T1, class T2>
-T1 div(T1 a, T2 b)
-{
-	return a / b;
-}
-
-template<class T1, class T2>
-T1 mod(T1 a, T2 b)
-{
-	return a % b;
-}
-
-template<class T1, class T2>
-T1 mult(T1 a, T2 b)
-{
-	return a * b;
-}
-
-template<class T1, class T2>
-T1(*op[5])(T1, T2) = { sum,diff,div,mod,mult };
-
-template<class T1, class T2>
-T1 calc(T1 a, T2 b, T1(*op)(T1, T2)) //T1 calc - выводит значение типа Т1 //(*op) передает функцию
-{
-	switch (--operation)
-	{
-	case '+':
-	{
-		cout << a << " + " << b << " = " << sum(a, b);
-		break;
-	}
-	case '-':
-	{
-		cout << a << " - " << b << " = " << diff(a, b);
-		break;
-	}
-	case '/':
-	{
-		cout << a << " / " << b << " = " << div(a, b);
-		break;
-	}
-	case '%':
-	{
-		cout << a << " % " << b << " = " << mod(a, b);
-		break;
-	}
-	case '*':
-	{
-		cout << a << " * " << b << " = " << mult(a, b);
-		break;
-	}
-	cout << "Wrong input" << endl;
-	}
-}
-
 int main(int argc, char* argv[])
 {
-	cout << argv[0];
+	if (argc != 7)
+	{
+		cout << "Wrong input! 1 ";
+		return 0;
+	}
+
+	bool op1 = 0;
+	bool op2 = 0;
+	bool op = 0;
+	for (int i = 1; i <= argc; ++i)
+	{
+		if (argv[i] == "--operand1")
+		{
+			if (op1 == 1)
+			{
+				cout << "Wrong input! 2 ";
+			}
+			op1 = 1;
+			cout << "op1 = 1\n";
+		}
+		if (argv[i] == "--operand2")
+		{
+			if (op2 == 1)
+			{
+				cout << "Wrong input! 3 ";
+			}
+			op2 = 1;
+			cout << "op2 = 1\n";
+		}
+		if (argv[i] == "--operator")
+		{
+			if (op == 1)
+			{
+				cout << "Wrong input! 4 ";
+			}
+			op = 1;
+			cout << "op = 1\n";
+		}
+	}
+	if (op1 == 0 || op2 == 0 || op == 0)
+	{
+		cout << op1 << ' ' << op2 << ' ' << op << endl;
+		cout << "Wrong input! 5 " << endl;
+	}
 
 
+
+	/*for (int j = 1; j <= argc; ++j)
+	{
+		int number1 = 0;
+		bool flag = 0;
+		int degree = 0;
+		int i = 0;
+		for (int i = 0; i <= strlen(argv[1]); ++i);
+		{
+			if (argv[1][i] == '.')
+			{
+				flag = 1;
+			}
+			else
+			{
+				number1 *= 10;
+				number1 += argv[1][i] - 48;
+			}
+			if (flag == 1)
+			{
+				degree++;
+			}
+
+		}
+		if (flag == 1)
+		{
+			number1 = (double)number1 / pow(10, degree);
+		}
+		cout << number1 << endl;
+	}*/
+
+	cout << "argc: " << argc << endl;
+	for (int i = 0; i < argc; ++i)
+	{
+		cout << i << ' ' << argv[i] << endl;
+	}
 	return 0;
 }
